8fe7cd1789acc9b2b3f854c12e62ecd9f72a4ec1
Konstantin Kruhmalev <kruhmalev.k@gmail.com>
2019-09-18 19:36:46 +0300

    Lab 2 - while_1 (ask_user())

---
+++ while_1.py
@@ -0,0 +1,26 @@
+"""
+Домашнее задание №1
+Цикл while: ask_user
+* Напишите функцию ask_user(), которая с помощью input() спрашивает 
+  пользователя “Как дела?”, пока он не ответит “Хорошо”
+   
+"""
+answers = {
+    "Как дела" : "Хорошо",
+    "Чем занимаешься" : "Программирую"
+}
+
+def ask_user():
+    user_say = ''
+    while user_say not in answers:
+        user_say = input('Задай вопрос? ')
+        if user_say in answers:
+            print(answers.get(user_say))
+            break
+        elif user_say == 'Пока':
+            print('Ну пока')
+            break
+        else:
+            print('Сам ты {}'.format(user_say))
+
+ask_user()
