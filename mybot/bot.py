f824684be689df0332894bad5e9e917cc812a2b2
Konstantin Kruhmalev <kruhmalev.k@gmail.com>
2019-09-27 21:03:43 +0300

    not working

--- mybot/bot.py
+++ mybot/bot.py
@@ -3,6 +3,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from bot_settings import apikey
 import logging
+import ephem
+import datetime as dt
 
 
 PROXY = {'proxy_url': 'socks5h://t3.learn.python.ru:1080',
@@ -13,9 +15,10 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
+date = dt.datetime.today().strftime("%d/%m/%Y")
 
 def greet_user(bot, update):
-    text = 'Вызван /start'
+    text = 'Вызван /start, вы можете ввести /planet x, где x - название планеты на Английском'
     logging.info(text)
     update.message.reply_text(text)
 
@@ -23,6 +26,11 @@ def talk_to_me(bot, update):
     user_text = update.message.text
     logging.info(user_text)
     update.message.reply_text(user_text)
+
+
+def planet(bot, update):
+    user_text = (update.message.text.split())
+    print(user_text)
 
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 
